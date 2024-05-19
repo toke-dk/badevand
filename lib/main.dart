80c65fd1b8b1aadee707a99bb4ee7dab177e7c2e
Toke Friis <tokefriis@gmail.com>
2024-05-19 11:40:07 +0200

    we can get the users geolocation if they permission

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:badevand/widgets/widget_to_map_icon.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
@@ -36,6 +37,46 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _selectedMenuIndex = 0;
 
+  Position? position;
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    position = await Geolocator.getCurrentPosition();
+    print("new position");
+  }
+
   Future<void> _initializeMarkers(List<Beach> beaches) async {
     Set<Marker> markerList = {};
 
@@ -55,6 +96,12 @@ class _MyAppState extends State<MyApp> {
                   indexBeach.comments != "" ? indexBeach.comments : null)));
     }
     context.read<GoogleMarkersProvider>().setMarkers(markerList);
+  }
+
+  @override
+  void initState() {
+    _determinePosition();
+    super.initState();
   }
 
   @override
