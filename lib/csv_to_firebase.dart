c61734c31af81b9120c21a40e7fe28f67a7113be
Toke Friis <tokefriis@gmail.com>
2024-06-03 12:32:06 +0200

    removed dublicate names

--- lib/csv_to_firebase.dart
+++ lib/csv_to_firebase.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:io';
 import 'dart:math' as math;
 import 'package:badevand/extenstions/numbers_extension.dart';
@@ -11,7 +12,10 @@ Future<void> main(List<String> arguments) async {
   List<List<dynamic>> rowsAsListOfValues =
       CsvToListConverter(fieldDelimiter: ";")
           .convert(await file.readAsString());
-  print(rowsAsListOfValues.skip(1).map((row) {
+  List<Map<String, dynamic>> result = rowsAsListOfValues.skip(1).map((row) {
+    String name = row[2].toString();
+    String municipality = row[13].toString();
+
     int zone = int.parse(row[4].toString());
     double xCords = row[5].toString().commaDecimalToDouble;
     double yCords = row[6].toString().commaDecimalToDouble;
@@ -24,6 +28,18 @@ Future<void> main(List<String> arguments) async {
     double lat = ddCords.latitude;
     double lon = ddCords.longitude;
 
-    return "$lat $lon";
-  }).toList()[6]);
+    return {"name": name, "lat": lat, "lon": lon, "municipality": municipality};
+  }).toList();
+
+  final Set names = result.map((e) => e["name"]).toSet();
+  result.retainWhere((x) => names.remove(x["name"]));
+  print(result.length);
+}
+
+bool areMapsEqual(Map map1, Map map2) {
+  if (map1.length != map2.length) return false;
+  for (var key in map1.keys) {
+    if (map1[key] != map2[key]) return false;
+  }
+  return true;
 }
