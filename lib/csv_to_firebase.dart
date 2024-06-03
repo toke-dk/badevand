b729d47030533d2dd479fa4336ed8ba00a9b3336
Toke Friis <tokefriis@gmail.com>
2024-06-03 12:01:52 +0200

    correct lat long conversion

--- lib/csv_to_firebase.dart
+++ lib/csv_to_firebase.dart
@@ -1,18 +1,31 @@
 import 'dart:io';
 import 'dart:math' as math;
+import 'package:badevand/extenstions/numbers_extension.dart';
 import 'package:badevand/extenstions/string_extension.dart';
 import 'package:csv/csv.dart';
+import 'package:coordinate_converter/coordinate_converter.dart';
 
 Future<void> main(List<String> arguments) async {
   final path = arguments[0];
   final file = File(path);
-  List<List<dynamic>> rowsAsListOfValues = CsvToListConverter(
-      fieldDelimiter: ";").convert(await file.readAsString());
+  List<List<dynamic>> rowsAsListOfValues =
+      CsvToListConverter(fieldDelimiter: ";")
+          .convert(await file.readAsString());
   print(rowsAsListOfValues.skip(1).map((row) {
-    String placeName = row[2].toString();
-    double yCords = y2lat(row[6].toString().commaDecimalToDouble);
-    return yCords;
-  }).toList()[0]);
+    int zone = int.parse(row[4].toString());
+    double xCords = row[5].toString().commaDecimalToDouble;
+    double yCords = row[6].toString().commaDecimalToDouble;
+
+    UTMCoordinates utmCords =
+        UTMCoordinates(x: xCords, y: yCords, zoneNumber: zone);
+
+    DDCoordinates ddCords = utmCords.toDD();
+
+    double lat = ddCords.latitude;
+    double lon = ddCords.longitude;
+
+    return "$lat $lon";
+  }).toList()[6]);
 }
 
 double y2lat(double y) {
--- lib/extenstions/numbers_extension.dart
+++ lib/extenstions/numbers_extension.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
-
-import '../models/beach.dart';
+import 'dart:math' as math;
 
 extension NumerExtension on num {
   String get myDoubleToString => toStringAsFixed(this % 1 == 0 ? 0 : 1);
@@ -11,5 +10,24 @@ extension NumerExtension on num {
 
   String get asMillimetersString => "$myDoubleToString mm";
 
-  double get toRadiansFromDegree => this*pi/180;
+  double get toRadiansFromDegree => this * pi / 180;
 }
+
+extension CoordinateTransform on num {
+  /// All conversions are from this source:
+  /// https://wiki.openstreetmap.org/wiki/Mercator#JavaScript
+
+  static const pi = math.pi;
+  static const radToDeg = 180 / pi;
+  static const degToRad = pi / 180;
+  static const r = 6378137;
+
+  double get y2lat => (2 * math.atan(math.exp(this / r)) - pi / 2) * radToDeg;
+
+  double get x2lon => radToDeg * (this / r);
+
+  double get lat2y => math.log(math.tan(pi / 4 + this * degToRad / 2) * r);
+
+  double get lon2x => this * degToRad * r;
+}
+
