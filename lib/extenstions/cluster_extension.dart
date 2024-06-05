22d24c4d53769696734e78efbab51a57ceab0c11
Toke Friis <tokefriis@gmail.com>
2024-06-05 14:34:05 +0200

    it has a infowindow at the marker at spot

--- lib/extenstions/cluster_extension.dart
+++ lib/extenstions/cluster_extension.dart
@@ -4,27 +4,31 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapMarker extends Clusterable {
   final String id;
   final LatLng position;
+  final String? title;
 
   MapMarker({
     required this.id,
     required this.position,
+    this.title,
     isCluster = false,
     clusterId,
     pointsSize,
     childMarkerId,
   }) : super(
-    markerId: id,
-    latitude: position.latitude,
-    longitude: position.longitude,
-    isCluster: isCluster,
-    clusterId: clusterId,
-    pointsSize: pointsSize,
-    childMarkerId: childMarkerId,
-  );Marker toMarker() => Marker(
-    markerId: MarkerId(id),
-    position: LatLng(
-      position.latitude,
-      position.longitude,
-    ),
-  );
+          markerId: id,
+          latitude: position.latitude,
+          longitude: position.longitude,
+          isCluster: isCluster,
+          clusterId: clusterId,
+          pointsSize: pointsSize,
+          childMarkerId: childMarkerId,
+        );
+
+  Marker toMarker() {
+    return Marker(
+      infoWindow: InfoWindow(title: title),
+      markerId: MarkerId(id),
+      position: position,
+    );
+  }
 }
