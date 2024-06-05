75143cdc9f61d41068d3061a7ed8532becd29b22
Toke Friis <tokefriis@gmail.com>
2024-06-05 19:58:39 +0200

    WIP: takes weather data from the meteo API

--- lib/models/meteo/weather_data.dart
+++ lib/models/meteo/weather_data.dart
@@ -1,22 +1,40 @@
 import 'package:badevand/models/meteo/date_info.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MeteoWeatherData {
-  LatLng location;
-  List<DateInfo> dateInfo;
+class MeteorologicalData {
+  List<DateInfo> temperatures;
+  List<DateInfo> precipitation;
+  List<DateInfo> windSpeeds;
+  List<DateInfo> windGusts;
+  List<DateInfo> windDirections;
+  List<DateInfo> uvIndexes;
+  List<DateInfo> weatherSymbols;
 
-  MeteoWeatherData({required this.location, required this.dateInfo});
+  MeteorologicalData(
+      {required this.temperatures,
+      required this.precipitation,
+      required this.windSpeeds,
+      required this.windGusts,
+      required this.windDirections,
+      required this.uvIndexes,
+      required this.weatherSymbols});
 
-  factory MeteoWeatherData.fromMap(List<dynamic> data, String parameter) {
+  factory MeteorologicalData.fromMeteoMap(List<dynamic> map) {
+    List<DateInfo> getDatesInfoFromString(String string) {
+      return (map
+              .firstWhere((e) => e["parameter"] == string)["coordinates"]
+              .first["dates"] as List<dynamic>)
+          .map((m) => DateInfo.fromMap(m))
+          .toList();
+    }
 
-    final List<dynamic> coordinateData = data.firstWhere(
-            (e) => e["parameter"] == parameter)["coordinates"];
-    final double lat = coordinateData.first["lat"] as double;
-    final double lon = coordinateData.first["lon"] as double;
-    return MeteoWeatherData(
-        location: LatLng(lat, lon),
-        dateInfo: (coordinateData.first["dates"] as List<dynamic>)
-            .map((e) => DateInfo.fromMap(e))
-            .toList());
+    return MeteorologicalData(
+        temperatures: getDatesInfoFromString("t_2m:C"),
+        precipitation: getDatesInfoFromString("precip_1h:mm"),
+        windSpeeds: getDatesInfoFromString("wind_speed_10m:ms"),
+        windGusts: getDatesInfoFromString("wind_gusts_10m_1h:ms"),
+        windDirections: getDatesInfoFromString("wind_dir_10m:d"),
+        uvIndexes: getDatesInfoFromString("uv:idx"),
+        weatherSymbols: getDatesInfoFromString("weather_symbol_1h:idx"));
   }
 }
--- lib/pages/beach_info/specs_widget.dart
+++ lib/pages/beach_info/specs_widget.dart
@@ -3,11 +3,14 @@ import 'dart:convert';
 import 'package:badevand/enums/water_quality.dart';
 import 'package:badevand/env/env.dart';
 import 'package:badevand/extenstions/date_extensions.dart';
+import 'package:badevand/models/wind_direction.dart';
+import 'package:badevand/providers/loading_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:gap/gap.dart';
+import 'package:provider/provider.dart';
 import '../../models/beach.dart';
 import 'package:http/http.dart' as http;
 
@@ -25,7 +28,10 @@ import '../../models/meteo/weather_data.dart';
 import 'beach_info_page.dart';
 
 class SpecsWidget extends StatefulWidget {
-  SpecsWidget({super.key, required this.beach, });
+  SpecsWidget({
+    super.key,
+    required this.beach,
+  });
 
   final Beach beach;
 
@@ -36,152 +42,65 @@ class SpecsWidget extends StatefulWidget {
 class _SpecsWidgetState extends State<SpecsWidget> {
   int? maxLines = 3;
 
-  int _selectedDateIndex = 0;
+  MeteorologicalData? _receivedData;
 
-  List<dynamic> _receivedData = [];
+  Future<void> initMeteorologicalData() async {
+    context.read<LoadingProvider>().toggleAppLoadingState(true);
+    await getWeatherData(widget.beach).then((result) {
+      setState(() {
+        _receivedData = result;
+      });
+      context.read<LoadingProvider>().toggleAppLoadingState(false);
+    });
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      initMeteorologicalData();
+    });
+    super.initState();
+  }
+
+  bool get _isAppLoading => context.watch<LoadingProvider>().getIsAppLoading;
 
   @override
   Widget build(BuildContext context) {
-
-    final TextTheme textTheme = Theme.of(context).textTheme;
-
-    final List<BeachSpecifications> specifications = widget.beach.beachSpecifications;
-
-    final BeachSpecifications? specificationForSelectedIndex =
-    specifications.isEmpty ? null : widget.beach.beachSpecifications[_selectedDateIndex];
-    
-    late BeachSpecifications? specsToday = widget.beach.getSpecsOfToday;
-
-    return specificationForSelectedIndex == null ? SizedBox.shrink() : Column(
-      children: [
-        Row(
-          children: [
-            specsToday?.weatherType?.icon ?? SizedBox.shrink(),
-            Gap(30),
-            Expanded(
-              child: Text(
-                overflow: TextOverflow.visible,
-                specsToday?.weatherType?.displayedText ??
-                    "Ukendt vejr",
-                style: textTheme.titleLarge,
-              ),
-            ),
-          ],
-        ),
-        const Gap(35),
-        Center(
-          child: CustomSlidingSegmentedControl(
-              innerPadding: const EdgeInsets.all(8),
-              customSegmentSettings: CustomSegmentSettings(
-                  splashColor: Colors.transparent,
-                  hoverColor: Colors.transparent),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: Theme.of(context)
-                      .colorScheme
-                      .inversePrimary
-                      .withAlpha(100)),
-              thumbDecoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(.3),
-                    blurRadius: 4.0,
-                    spreadRadius: 1.0,
-                    offset: const Offset(
-                      0.0,
-                      2.0,
-                    ),
-                  ),
-                ],
-              ),
-              children: Map<int, Widget>.fromEntries(specifications
-                  .asMap()
-                  .entries
-                  .map((e) => MapEntry(e.key,
-                  Text(e.value.dataDate.dateAsRelativeString)))),
-              onValueChanged: (newVal) {
-                setState(() {
-                  _selectedDateIndex = newVal;
-                });
-              }),
-        ),
-        Gap(10),
-        ListTile(
-          leading: Icon(Icons.date_range),
-          title:
-          Text(specificationForSelectedIndex.dataDate.myDateFormat),
-          subtitle: Text("Dato"),
-        ),
-        Divider(),
-        ListTile(
-          leading: specificationForSelectedIndex.waterQualityType.flag,
-          title: Text(
-              specificationForSelectedIndex.waterQualityType.description),
-          subtitle: Text("Vandkvalitet"),
-        ),
-        ListTile(
-          leading: specificationForSelectedIndex.weatherType?.icon ??
-              Icon(Icons.question_mark),
-          title: Text(
-              specificationForSelectedIndex.weatherType?.displayedText ??
-                  "Ukendt vejrtype"),
-        ),
-        ListTile(
-          leading: Icon(Icons.water_drop_outlined),
-          title: Text(specificationForSelectedIndex
-              .waterTemperature.asCelsiusTemperature),
-          subtitle: Text("Vandtemperatur"),
-        ),
-        ListTile(
-          leading: Icon(Icons.thermostat),
-          title: Text(specificationForSelectedIndex
-              .airTemperature.asCelsiusTemperature),
-          subtitle: Text("Lufttemperatur"),
-        ),
-        ListTile(
-          leading: specificationForSelectedIndex
-              .windDirection?.getChildWidget ??
-              const Icon(Icons.question_mark),
-          title: Text(
-              specificationForSelectedIndex.windSpeed?.asMeterPerSecond ??
-                  "ingen informationer"),
-          subtitle: Text("Vind"),
-        ),
-        ListTile(
-          leading: Icon(WeatherIcons.rain),
-          title: Text(specificationForSelectedIndex
-              .precipitation?.asMillimetersString ??
-              "ingen informationer"),
-          subtitle: Text("Nedbør"),
-        ),
-        OutlinedButton(
-            onPressed: () async {
-              _receivedData = await getWeatherData();
-              setState(() {});
-            },
-            child: Text("Get data")),
-        OutlinedButton(
-            onPressed: () async {
-              final temperatures = _receivedData.firstWhere(
-                      (e) => e["parameter"] == "t_2m:C")["coordinates"].first["dates"].first["value"];
-              print(DateTime.now().meteoDateFormat);
-              print(MeteoWeatherData.fromMap(_receivedData, "t_2m:C"));
-            },
-            child: Text(
-                "Manipulate data ${_receivedData.isEmpty ? '(tom)' : ''}"))
-      ],
-    );
+    if (_isAppLoading) {
+      return Center(child: CircularProgressIndicator());
+    } else {
+      return Column(
+        children: [
+          _receivedData == null
+              ? SizedBox.shrink()
+              : ListTile(
+                  leading: Icon(Icons.thermostat),
+                  title: Text(_receivedData!
+                      .temperatures.first.value.asCelsiusTemperature),
+                  subtitle: Text("Lufttemperatur"),
+                ),
+          _receivedData == null
+              ? SizedBox.shrink()
+              : ListTile(
+                  leading: WindDirection(
+                          angle: _receivedData!.windDirections.first.value)
+                      .getChildWidget,
+                  title: Text(
+                      _receivedData!.windSpeeds.first.value.asMeterPerSecond),
+                  subtitle: Text("Vind"),
+                ),
+        ],
+      );
+    }
   }
 }
 
-Future<List<dynamic>> getWeatherData() async {
+Future<MeteorologicalData> getWeatherData(Beach beach) async {
   final DateTime firstDate = DateTime.now().subtract(1.days);
   final DateTime lastDate = DateTime.now().add(8.days);
 
   final url = Uri.parse(
-      'https://api.meteomatics.com/${firstDate.meteoDateFormat}--${lastDate.meteoDateFormat}:PT30M/weather_symbol_1h:idx,t_2m:C,precip_1h:mm,wind_speed_10m:ms/55.867298,11.460067/json');
+      'https://api.meteomatics.com/${firstDate.meteoDateFormat}--${lastDate.meteoDateFormat}:PT30M/weather_symbol_1h:idx,t_2m:C,precip_1h:mm,wind_speed_10m:ms,wind_dir_10m:d,uv:idx,wind_gusts_10m_1h:ms/${beach.position.latitude},${beach.position.longitude}/json');
 
   // final response = await http.get(url);
 
@@ -199,7 +118,7 @@ Future<List<dynamic>> getWeatherData() async {
   if (response.statusCode == 200) {
     final List<dynamic> data = jsonDecode(response.body)["data"];
     print(data);
-    return data;
+    return MeteorologicalData.fromMeteoMap(data);
   } else {
     // Handle error scenario
     throw Exception('Could not find the data from the link');
