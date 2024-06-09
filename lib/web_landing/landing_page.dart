a8db491c5977ce71808562d2a5cafa287e3ddb59
Toke Friis <tokefriis@gmail.com>
2024-06-09 22:23:44 +0200

    refactor

--- lib/web_landing/landing_page.dart
+++ lib/web_landing/landing_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'header.dart';
+
 class WebLandingPage extends StatelessWidget {
   const WebLandingPage({super.key});
 
@@ -26,83 +28,14 @@ class WebLandingPage extends StatelessWidget {
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Colors.blue[300]!, Colors.blue[900]!])),
-            child: Align(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                constraints: BoxConstraints(
-                  maxWidth: 1000,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Gap(30),
-                    Text(
-                      "Vil du være tester af Dansk Badevand?",
-                      style: textTheme.displaySmall!.copyWith(
-                        color: Colors.white,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    Gap(15),
-                    Text(
-                      "Tilmeld dig testgruppen på nedenstående felt",
-                      style: textTheme.bodyLarge!.copyWith(color: Colors.white),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: Container(
-                        color: Colors.white,
-                        padding: EdgeInsets.all(15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Bliv gratis en del af testgruppen".toUpperCase(),
-                              style: textTheme.titleLarge!
-                                  .copyWith(color: colorScheme.primary),
-                            ),
-                            Gap(10),
-                            Container(
-                              padding: EdgeInsets.all(8),
-                              color: Colors.grey[100],
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  labelText: "Skriv din email her*",
-                                ),
-                              ),
-                            ),
-                            Gap(20),
-                            FilledButton(
-                              onPressed: () {},
-                              child: Text("Tilmeld"),
-                              style: ButtonStyle(
-                                  shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.zero))),
-                            ),
-                            Gap(15),
-                            Text(
-                              "Tak for at tilmelde",
-                              style: textTheme.bodySmall,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+          Header(
+            textTheme: textTheme,
+            colorScheme: colorScheme,
           ),
         ],
       ),
     );
   }
 }
+
+
